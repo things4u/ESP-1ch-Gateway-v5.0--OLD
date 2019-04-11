72c7af6eef7ce9dcd75af6caa6ab6622d1151352
Gustavo F Nobrega <gustavofn@gmail.com>
2019-04-11 15:08:30 -0700

    Fixed the floating point precision problem with lorawan stack
    Fixed the floating point precision problem with lorawan stack

--- ESP-sc-gway/_txRx.ino
+++ ESP-sc-gway/_txRx.ino
@@ -488,7 +488,9 @@ int buildPacket(uint32_t tmst, uint8_t *buff_up, struct LoraUp LoraUp, bool inte
 	}
 #endif
 	buff_index += j;
-	ftoa((double)freq/1000000,cfreq,6);					// XXX This can be done better
+	double iifreq;
+  	iifreq = freq/1000000;
+	ftoa(iifreq,cfreq,6);					// Fixed the floating point precision problem with lorawan stack
 	j = snprintf((char *)(buff_up + buff_index), TX_BUFF_SIZE-buff_index, ",\"chan\":%1u,\"rfch\":%1u,\"freq\":%s", 0, 0, cfreq);
 	buff_index += j;
 	memcpy((void *)(buff_up + buff_index), (void *)",\"stat\":1", 9);
